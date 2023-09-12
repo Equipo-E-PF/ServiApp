4e64d349ef4b0ff867bbcbf7d77607da01cace82
Mari1398 <118990729+Mari1398@users.noreply.github.com>
2023-09-12 12:21:41 -0500

    Correcion de vinculacion front, creacion de fragment:Navbar

---
+++ ServiApp/src/main/java/com/egg/ServiApp/controladores/PortalControlador.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.egg.ServiApp.controladores;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author catal
+ */
+@Controller
+@RequestMapping("/")
+public class PortalControlador {
+     @GetMapping("/")
+    public String index(ModelMap model) {
+        return "index.html";
+    }
+}
