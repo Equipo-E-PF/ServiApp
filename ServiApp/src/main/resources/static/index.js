281dfbab91aaf05e3e1ee4ef62bc567fb8c7bd32
Jeff <76017033+jeffersonR91@users.noreply.github.com>
2023-09-15 15:48:22 -0500

    solucion de los menus desplegables, pagina del login.html y redireccionamento al index desde el el logo serviApp

--- ServiApp/src/main/resources/static/index.js
+++ ServiApp/src/main/resources/static/index.js
@@ -1,15 +1,27 @@
 const btn = document.getElementById("servicios");
 const menu = document.getElementById("menu");
 
-btn.addEventListener('click', ()=>{
+btn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
-    
-})
+    menuReg.classList.add('hidden');
+});
 
 const btnReg = document.getElementById("reg");
 const menuReg = document.getElementById("menuReg");
 
-btnReg.addEventListener('click', ()=>{
+btnReg.addEventListener('click', () => {
     menuReg.classList.toggle('hidden');
-    
-})
+    menu.classList.add('hidden');
+
+});
+
+document.addEventListener('click', (event) => {
+    const menu = document.getElementById('menu');
+    const menuReg = document.getElementById('menuReg'); 
+
+    if (!menu.contains(event.target) && !btn.contains(event.target) &&
+        !menuReg.contains(event.target) && !btnReg.contains(event.target)) {
+        menu.classList.add('hidden');
+        menuReg.classList.add('hidden');
+    }
+});
