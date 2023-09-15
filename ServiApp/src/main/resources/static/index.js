74df8c7f6f21c703751cdd2b8b1aaa8e5773c5ba
Rodrigo Pascual <119248752+Rodrigo-Franco-Pascual@users.noreply.github.com>
2023-09-14 22:55:20 -0300

    Pagina de Perfil de Usuario

--- ServiApp/src/main/resources/static/index.js
+++ ServiApp/src/main/resources/static/index.js
@@ -1,6 +1,11 @@
+
+
 const btn = document.getElementById("servicios");
 const menu = document.getElementById("menu");
 
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
 btn.addEventListener('click', ()=>{
     menu.classList.toggle('hidden');
     
@@ -13,3 +18,45 @@ btnReg.addEventListener('click', ()=>{
     menuReg.classList.toggle('hidden');
     
 })
+
+
+const $usuario = document.querySelector("#usuarios");
+const $servicios = document.querySelector("#servicioss");
+const $botonesUsuario = document.querySelector("#botones-usuario");
+const $botonesServicio = document.querySelector("#botones-servicio");
+
+$usuario.onclick = function () {
+  $botonesUsuario.classList.toggle("hidden");
+  if ($botonesServicio.className === "") {
+    $botonesServicio.className = "hidden";
+  }
+};
+
+$servicios.onclick = function () {
+  $botonesServicio.classList.toggle("hidden");
+  if ($botonesUsuario.className === "") {
+    $botonesUsuario.className = "hidden";
+  }
+};
+
+const $crearUsuario = document.querySelector("#crear-usuario");
+const $modificarUsuario = document.querySelector("#modificar-usuario");
+const $crearServicio = document.querySelector("#crear-servicio");
+const $modificarServicio = document.querySelector("#modificar-servicio");
+
+$crearUsuario.addEventListener(
+  "click",
+  () => (window.location.href = "https:www.google.com")
+);
+$modificarUsuario.addEventListener(
+  "click",
+  () => (window.location.href = "https:www.google.com")
+);
+$crearServicio.addEventListener(
+  "click",
+  () => (window.location.href = "https:www.facebook.com")
+);
+$modificarServicio.addEventListener(
+  "click",
+  () => (window.location.href = "https:www.facebook.com")
+);
