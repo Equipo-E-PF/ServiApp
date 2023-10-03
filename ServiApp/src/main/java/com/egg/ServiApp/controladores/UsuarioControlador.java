9d4deb1e5d19494f91a34b818a324fc5f61d89e8
Juan <juanpa.angulo98@hotmail.com>
2023-10-03 11:29:26 -0500

    Controlador completo usuario-proveedor contrataciones

--- ServiApp/src/main/java/com/egg/ServiApp/controladores/UsuarioControlador.java
+++ ServiApp/src/main/java/com/egg/ServiApp/controladores/UsuarioControlador.java
@@ -4,19 +4,16 @@ import com.egg.ServiApp.entidades.Especialidad;
 import com.egg.ServiApp.entidades.Usuario;
 import com.egg.ServiApp.entidades.Trabajo;
 import com.egg.ServiApp.enumeraciones.Estado;
+import com.egg.ServiApp.enumeraciones.Rol;
 
 import com.egg.ServiApp.servicios.calificacionServicio;
 import com.egg.ServiApp.servicios.especialidadServicio;
 import com.egg.ServiApp.servicios.trabajoServicio;
 import com.egg.ServiApp.servicios.usuarioServicio;
 import excepciones.miException;
-import java.security.Principal;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -45,7 +43,7 @@ public class UsuarioControlador {
     private calificacionServicio calificacionServicio;
     @Autowired
     private especialidadServicio especialidadServicio;
-    
+
     @GetMapping("/perfil/{id}")
     public String cargarPerfil(@PathVariable String id, ModelMap modelo) {
         modelo.put("user", usuarioServicio.ProviderById(id));
@@ -101,7 +99,7 @@ public class UsuarioControlador {
             model.put("exito", "Se modificó correctamente la contraseña");
 
             return "redirect:../perfil/{id}";
-        }catch (miException ex) {
+        } catch (miException ex) {
 
             model.put("error", ex.getMessage());
             return "redirect:../perfil";
@@ -165,25 +163,57 @@ public class UsuarioControlador {
         return "/logout";
     }
 
-    @GetMapping("/contrataciones/{id}")
-    public String contrataciones(@PathVariable String id, ModelMap model) {
-        
-            // Trabajo creado en estado Pendiente con x proveedor
-            List<Trabajo> listaUsuariosPen = trabajoServicio.TrabajoPorProveedorEstado(id, Estado.PENDIENTE);
-            model.addAttribute("usuariosPendientes", listaUsuariosPen);
+    @GetMapping("/contrataciones")
+    public String contrataciones(@SessionAttribute("usersession") Usuario usuario, ModelMap model) {
+        List<Especialidad> especialidades = especialidadServicio.listarEspecialidades();
+        model.addAttribute("especialidades", especialidades);
 
-            // Trabajo en curso en estado Pendiente con x proveedor
-            List<Trabajo> listaUsuariosAcep = trabajoServicio.TrabajoPorProveedorEstado(id, Estado.ACEPTADO);
-            model.addAttribute("usuariosAceptados", listaUsuariosAcep);
+        if (usuario != null) {
 
-            // Trabajo terminado en estado Completo con x proveedor
-            List<Trabajo> listaUsuariosCom = trabajoServicio.TrabajoPorProveedorEstado(id, Estado.FINALIZADO);
-            model.addAttribute("usuariosCompleto", listaUsuariosCom);
+            if (usuario.getRol().equals(Rol.PROVEEDOR)) {
+                
+                // Trabajo creado en estado Pendiente con x proveedor
+                List<Trabajo> listaUsuariosPen = trabajoServicio.TrabajoPorProveedorEstado(usuario.getId(), Estado.PENDIENTE);
+                model.addAttribute("usuariosPendientes", listaUsuariosPen);
 
-            // Lista de trabajos
-            List<Trabajo> trabajo = trabajoServicio.listarTrabajoPorProveedor(id);
-            model.addAttribute("trabajo", trabajo);
+                // Trabajo en curso en estado Aceptado con x proveedor
+                List<Trabajo> listaUsuariosAcep = trabajoServicio.TrabajoPorProveedorEstado(usuario.getId(), Estado.ACEPTADO);
+                model.addAttribute("usuariosAceptados", listaUsuariosAcep);
+
+                // Trabajo terminado en estado Completo con x proveedor
+                List<Trabajo> listaUsuariosCom = trabajoServicio.TrabajoPorProveedorEstado(usuario.getId(), Estado.FINALIZADO);
+                model.addAttribute("usuariosCompleto", listaUsuariosCom);
+
+                // Lista de trabajos calificados
+                List<Trabajo> trabajoCalificado = trabajoServicio.TrabajoCalificadosProveedor(usuario.getId());
+                model.addAttribute("trabajoCalificado", trabajoCalificado);
+            }
+
+            if (usuario.getRol().equals(Rol.USUARIO)) {
+                
+                // Trabajo creado en estado Pendiente con x proveedor
+                List<Trabajo> listaUsuariosPen = trabajoServicio.TrabajoPorUsuarioEstado(usuario.getId(), Estado.PENDIENTE);
+                model.addAttribute("usuariosPendientes", listaUsuariosPen);
+
+                // Trabajo en curso en estado Aceptado con x proveedor
+                List<Trabajo> listaUsuariosAcep = trabajoServicio.TrabajoPorUsuarioEstado(usuario.getId(), Estado.ACEPTADO);
+                model.addAttribute("usuariosAceptados", listaUsuariosAcep);
+
+                // Trabajo terminado en estado Completo con x proveedor
+                List<Trabajo> listaUsuariosCom = trabajoServicio.TrabajoPorUsuarioEstado(usuario.getId(), Estado.FINALIZADO);
+                model.addAttribute("usuariosCompleto", listaUsuariosCom);
+
+                // Lista de trabajos calificados
+                List<Trabajo> trabajoCalificado = trabajoServicio.TrabajoCalificadosUsuario(usuario.getId());
+                model.addAttribute("trabajoCalificado", trabajoCalificado);
+            }
+            
             return "contrataciones.html";
+            
+        } 
+        else {
+            return "redirect:/login";
+        }
     }
 
     @PostMapping("/contrataciones/aceptar/{id}")
@@ -194,9 +224,9 @@ public class UsuarioControlador {
         } catch (miException e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
         }
-        return "redirect:/usuario/contrataciones/{id}";
+        return "redirect:/usuario/contrataciones";
     }
-    
+
     @PostMapping("/contrataciones/rechazar/{id}")
     public String rechazar(@PathVariable String id, RedirectAttributes redirectAttributes) {
         try {
@@ -205,9 +235,9 @@ public class UsuarioControlador {
         } catch (miException e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
         }
-        return "redirect:/usuario/contrataciones/{id}";
+        return "redirect:/usuario/contrataciones";
     }
-    
+
     @PostMapping("/contrataciones/finalizar/{id}")
     public String finalizar(@PathVariable String id, RedirectAttributes redirectAttributes) {
         try {
@@ -216,7 +246,7 @@ public class UsuarioControlador {
         } catch (miException e) {
             redirectAttributes.addFlashAttribute("error", e.getMessage());
         }
-        return "redirect:/usuario/contrataciones/{id}";
+        return "redirect:/usuario/contrataciones";
     }
 
 }
--- ServiApp/src/main/java/com/egg/ServiApp/repositorio/trabajoRepositorio.java
+++ ServiApp/src/main/java/com/egg/ServiApp/repositorio/trabajoRepositorio.java
@@ -37,4 +37,13 @@ public interface trabajoRepositorio extends JpaRepository<Trabajo, String> {
     @Query("SELECT DISTINCT t FROM Trabajo t WHERE t.proveedor.id = :proveedorId AND t.estado = :estado")
     public List<Trabajo> TrabajoPorProveedorEstado(@Param("proveedorId") String proveedorId, @Param("estado") Estado estado);
 
+    @Query("SELECT DISTINCT t FROM Trabajo t WHERE t.usuario.id = :usuarioId AND t.estado = :estado")
+    public List<Trabajo> TrabajoPorUsuarioEstado(@Param("usuarioId") String usuarioId, @Param("estado") Estado estado);
+    
+    @Query("SELECT DISTINCT t FROM Trabajo t WHERE t.proveedor.id = :proveedorId AND t.calificacion.contenido != null ")
+    public List<Trabajo> TrabajoCalificadosProveedor (@Param("proveedorId") String proveedorId);
+    
+    @Query("SELECT DISTINCT t FROM Trabajo t WHERE t.usuario.id = :usuarioId AND t.calificacion.contenido != null ")
+    public List<Trabajo> TrabajoCalificadosUsuario (@Param("usuarioId") String usuarioId);
+
 }
